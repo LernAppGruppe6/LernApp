e39acbfda7b1e4585ba02a3c50396d1f30bae5e1
Jannik Röhm <jr121@hdm-stuttgart.de>
2022-10-15 23:24:17 +0200

    tests that backend works

--- backend/app.py
+++ backend/app.py
@@ -12,35 +12,33 @@ Enable all resources for cross origin resource sharing to ease development.
 """
 CORS(app, resources=r'*')
 
-"""
-Connect to MySql Server
-Should be moved to the DataMappers
-"""
-conn = mysql.connector.connect(
-    user='root',
-    password='root',
-    host='db',
-    database='app',
-    use_pure=False
-)
-
 @app.route("/")
 def hello_world():
+    """
+    Connect to MySql Server
+    Should be moved to the DataMappers
+    """
+    conn = mysql.connector.connect(
+        user='root',
+        password='root',
+        host='db',
+        database='app',
+        use_pure=False
+    )
+
     cursor = conn.cursor()
     cursor.execute("SELECT email FROM login")
     logins = cursor.fetchall()
     cursor.close()
+    conn.close()
     
     res = ""
     
     for login in logins:
       res = res + login[0]
     
-    
     return "<p>Hello, World!</p>"+res
     
 # main driver function
 if __name__ == "__main__":
     app.run()
-    
-    conn.close()
