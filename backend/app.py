957dc1251b8136904c5a98efaa428c4e2f7680b2
Jannik Röhm <jr121@hdm-stuttgart.de>
2022-10-15 20:21:08 +0200

    connect to database

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,46 @@
 from flask import Flask
 
+import os
+import mysql.connector
+from flask_cors import CORS
+
 app = Flask(__name__)
+
+"""
+Enable all resources for cross origin resource sharing to ease development.
+- Javascript should access the api
+"""
+CORS(app, resources=r'*')
+
+"""
+Connect to MySql Server
+Should be moved to the DataMappers
+"""
+conn = mysql.connector.connect(
+    user='root',
+    password='root',
+    host='db',
+    database='app',
+    use_pure=False
+)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    cursor = conn.cursor()
+    cursor.execute("SELECT email FROM login")
+    logins = cursor.fetchall()
+    cursor.close()
+    
+    res = ""
+    
+    for login in logins:
+      res = res + login[0]
+    
+    
+    return "<p>Hello, World!</p>"+res
     
 # main driver function
 if __name__ == "__main__":
     app.run()
+    
+    conn.close()
