bdbded70f8bbe0174cba972b630c9ccf36c9017e
Jannik Röhm <jr121@hdm-stuttgart.de>
2022-11-23 18:44:46 +0100

    Create Student_Knowledge_Mapper

---
+++ backend/server/db/StudentKnowledgeMapper.py
@@ -0,0 +1,11 @@
+from . import Mapper
+from . import StudentMapper
+from . import SubjectMapper
+from uuid import UUID
+
+class StudentKnowledgeMapper(Mapper):
+    """
+    """
+
+    def __init__(self):
+        super().__init__()
