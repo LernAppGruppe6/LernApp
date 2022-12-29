c01f8b3e30c94440d693dfda38132f88808b9f1d
Jasmin Krewenka <102183443+krewenkajasmin@users.noreply.github.com>
2022-12-29 16:45:29 +0100

    Update Dashboard.js

--- frontend/lern-app/src/pages/Dashboard.js
+++ frontend/lern-app/src/pages/Dashboard.js
@@ -1,25 +1,3 @@
-import React from "react";
-import Container from "@mui/material/Container";
-import { Box } from "@mui/system";
-
-
-class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-  render() {
-    return (
-      <Container style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }} maxWidth={false}>
-        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginBottom: 1, flexWrap: 'wrap' }}>
-        </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginBottom: 1, flexWrap: 'wrap' }} >
-          <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', marginLeft: 2 }}>
-          </Box>
-        </Box>
-      </Container>
-    );
-  }
+export default function Dashboard () {
+  return <h1>Dashboard</h1>
 }
-export default Dashboard;
