3d2775dfccb39e464cd41af086137698acf80cfd
Jasmin Krewenka <102183443+krewenkajasmin@users.noreply.github.com>
2022-12-30 15:01:01 +0100

    Create Component: Message

--- frontend/lern-app/src/pages/Message.js
+++ frontend/lern-app/src/pages/Message.js
@@ -1,24 +1,79 @@
-import React from "react";
-import Container from "@mui/material/Container";
-import { Box } from "@mui/system";
+import * as React from 'react';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Typography from '@mui/material/Typography';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import SendIcon from '@mui/icons-material/Send';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
 
 
 
-class Message extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { seconds: 0 };
-  }
-
-
-  render() {
-    return (
-      <Container style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }} maxWidth={false}>
-        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginBottom: 1, flexWrap: 'wrap' }}>
-        </Box>
-      </Container>
-    );
-  }
+export default function Message() {
+  return (
+    <>
+      <MenuList>
+        <MenuItem>
+          <ListItemIcon>
+            <SendIcon fontSize="small" />
+          </ListItemIcon>
+          <Typography variant="inherit">sent messages</Typography>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <PriorityHighIcon fontSize="small" />
+          </ListItemIcon>
+          <Typography variant="inherit">highest priority </Typography>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <DraftsIcon fontSize="small" />
+          </ListItemIcon>
+          <Typography variant="inherit" noWrap>
+          read messages
+          </Typography>
+        </MenuItem>
+      </MenuList>
+         <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+         <nav aria-label="main mailbox folders">
+           <List>
+             <ListItem disablePadding>
+               <ListItemButton>
+                 <ListItemIcon>
+                   <InboxIcon />
+                 </ListItemIcon>
+                 <ListItemText primary="Inbox" />
+               </ListItemButton>
+             </ListItem>
+             <ListItem disablePadding>
+                 <ListItemIcon>
+                 </ListItemIcon>
+             </ListItem>
+           </List>
+         </nav>
+         <Divider />
+         <nav aria-label="secondary mailbox folders">
+           <List>
+             <ListItem disablePadding>
+               <ListItemButton>
+               </ListItemButton>
+             </ListItem>
+             <ListItem disablePadding>
+               <ListItemButton component="a" href="#simple-list">
+               </ListItemButton>
+             </ListItem>
+           </List>
+         </nav>
+       </Box>
+       </>
+  );
 }
 
-export default Message;
+
