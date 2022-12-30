b1071a4a26a2d1e89804df6387597dedfcce8a0c
Jasmin Krewenka <102183443+krewenkajasmin@users.noreply.github.com>
2022-12-30 15:01:52 +0100

    Create Dashboard List

---
+++ frontend/lern-app/src/components/DashboardList.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function BasicButtons() {
+  return (
+      <Button variant="outlined">Outlined</Button>
+    
+  );
+}
