36984a368d831e1f461f6bf589cddd8455c8fd3f
Jasmin Krewenka <102183443+krewenkajasmin@users.noreply.github.com>
2023-01-03 20:46:17 +0100

    Update MessageList.js

--- frontend/lern-app/src/components/MessageList.js
+++ frontend/lern-app/src/components/MessageList.js
@@ -1,13 +1,63 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 
-export default function MessageList() {
+function MessageList(
+    name: name,
+    status: name,
+    messagecontent: text,
+    priority: name,
+  ) {
+  return { status, name, messagecontent, priority };
+}
+
+const rows = [
+    MessageList('Lisa Müller', 'unread', 'Hello, .... ', 'high'),
+    MessageList('Bertha Meier', 'unread', 'Hi, my name is....', 'high'),
+    MessageList('Hans Schneider', 'read', 'Hi....', 'normal'),
+    MessageList('Peter Schmitt', 'read', 'yes...', 'normal'),
+    MessageList('Gerta Trumpf', ' read', 'no...', 'low'),
+];
+
+export default function DenseTable() {
   return (
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
+    <>
+    <Typography gutterBottom variant="h5" component="div">
+        All Messages
+       </Typography>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Status </TableCell>
+            <TableCell align="right">Message Content</TableCell>
+            <TableCell align="right">Priority</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.status}</TableCell>
+              <TableCell align="right">{row.messagecontent}</TableCell>
+              <TableCell align="right">{row.priority}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </>
   );
 }
