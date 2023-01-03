17cca493f302cb63c0afa283b8d50ef91d1fbf59
Jasmin Krewenka <102183443+krewenkajasmin@users.noreply.github.com>
2023-01-03 20:46:51 +0100

    Update LernGroup.js

--- frontend/lern-app/src/pages/LernGroup.js
+++ frontend/lern-app/src/pages/LernGroup.js
@@ -1,106 +1,14 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Checkbox from '@mui/material/Checkbox';
-import { visuallyHidden } from '@mui/utils';
-import Button from '@mui/material/Button';
+import LearnGroupList from '../components/LearnGroupList'
 
 
-const headCells = [
-    {
-        id: 'subject',
-        numeric: false,
-        disablePadding: true,
-        label: 'Subject',
-    },
-    {
-        id: 'max group_member',
-        numeric: false,
-        disablePadding: true,
-        label: 'Max Groupmember',
-    },
-    {
-        id: 'group chat',
-        numeric: false,
-        disablePadding: true,
-        label: 'Group Chat',
-    },
-    {
-        id: 'place_learning',
-        numeric: false,
-        disablePadding: true,
-        label: 'Place of Learning',
-    },
-    {
-        id: 'groupmembers',
-        numeric: false,
-        disablePadding: true,
-        label: 'GroupMembers',
-    },
-];
-
-export default function LearnGroup(props) {
-    const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
-    const createSortHandler = (property) => (event) => {
-        event.preventDefault();
-        onRequestSort(event, property);
-    };
-
+export default function LearnGroup() {
   return (
     <>
 
-    <Button variant="outlined">+ New Learngroup</Button>
-    
-    <TableHead>
-      <TableRow>
-          <TableCell padding="checkbox">
-            <Checkbox
-              color="primary"
-              indeterminate={numSelected > 0 && numSelected < rowCount}
-              checked={rowCount > 0 && numSelected === rowCount}
-              onChange={onSelectAllClick}
-              >
-            </Checkbox>
-          </TableCell>
-          {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align='left'
-            padding={headCell.disablePadding ? 'none' : 'normal'}
-            sortDirection={orderBy === headCell.id ? order : false}>
-              <TableSortLabel
-                active={orderBy === headCell.id}
-                direction={orderBy === headCell.id ? order : 'asc'}
-                onClick={createSortHandler(headCell.id)}
-                sx={{fontWeight: "bold"}}>
-                  {headCell.label}
-                  {orderBy === headCell.id ? (
-                    <Box component="span" sx={visuallyHidden}>
-                      {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                    </Box>
-                  ) : null}
-              </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
+    < LearnGroupList />
     </>
+
+
   );
 }
-
-LearnGroup.propTypes = {
-    numSelected: PropTypes.number.isRequired,
-    onRequestSort: PropTypes.func.isRequired,
-    onSelectAllClick: PropTypes.func.isRequired,
-    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-    orderBy: PropTypes.string.isRequired,
-    rowCount: PropTypes.number.isRequired,
-};
-
-
-
-
