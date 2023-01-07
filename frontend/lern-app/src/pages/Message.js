a4827aa57e5009642f3f58abc00f480bfb7f8083
Jasmin Krewenka <102183443+krewenkajasmin@users.noreply.github.com>
2023-01-07 10:19:08 +0100

    Update Message.js

--- frontend/lern-app/src/pages/Message.js
+++ frontend/lern-app/src/pages/Message.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import HeaderMessage from '../components/HeaderMessage';
-import MessageList from '../components/MessageList'
+import MessageList from '../components/MessageList';
+import Requests from '../components/Requests';
+
 export default function Message() {
   return (
     <>
     <HeaderMessage />
+    <Requests />
     <MessageList />
   
 
